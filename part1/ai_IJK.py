af2bbe336a0e974ddb00732dd1a882f9ab07d589
Supe, Neha <nehasupe@iu.edu>
2019-11-08 10:43:10 -0500

    Updated code from instructors

--- part1/ai_IJK.py
+++ part1/ai_IJK.py
@@ -1,132 +1,88 @@
 #!/usr/local/bin/python3
 
 """
-This is where you should write your AI code!
+@author: Abhilash Kuhikar, October 2019
 
-Authors: Kelly Wheeler (kellwhee), Neha Supe (nehasupe), Bobby Rathore (brathore)
+THIS IS VERSION v2.0, updated 11/1/2019.
 
-Based on skeleton code by Abhilash Kuhikar, October 2019
+./ijk.py : Interface to an IJK game.
+
+Usage:
+    ./ijk.py player1 player2 mode
+
+where:
+
+    player1 and player2 are either "ai" or "human"
+    mode is either "det" for deterministic or "nondet" for nondetermistic
+
+You may want to modify this file for debugging purposes, but all
+of your AI logic should be in solve_IJK.py.
 """
 
-from logic_IJK import Game_IJK
+import time, copy
 import random
-import math
+import sys
+import ai_IJK
+import human_IJK
 
-# Suggests next move to be played by the current player given the current game
-#
-# inputs:
-#     game : Current state of the game
-#
-# This function should analyze the current state of the game and determine the
-# best move for the current player. It should then call "yield" on that move.
+from logic_IJK import Game_IJK, initialGame
 
-def isGameOver(board):
-#None game is not over
-#-1: game is a draw
-#0: player + wins
-#1: player - wins
-    moreMoves = -1
-    for i in range(len(board)):
-        for j in range(len(board[0])):
-            if board[i][j] == 'K':
-                return 0
-            if board[i][j] == 'k':
-                return 1
-            if board[i][j] == ' ':
-                moreMoves = None
-    return moreMoves
+def IJK(player1, player2, deterministic, timeout=60, max_moves=30000):
+    game = initialGame(6, '+', deterministic)
 
-#+:1
-#-:-1
-#draw:0
-def minimax(game, board, depth, player):
-    
-#is the game over, return draw or winner
-    terminate = isGameOver(board)
-    if terminate == 0:
-        #player + wins
-        return None, 1
-    if terminate == 1:
-        #player - wins
-        return None, -1
-    if terminate == -1:
-        #draw
-        return None, 0
+    game.printGame()
 
-    if depth == 0:
-        #hit depth game over
-        return None, 0
-
-    values = []
     moves = []
-    for move in ["U", "D", "L", "R", "S"]:
-        child = game.makeMove(move)
-        value = minimax(game, child, depth - 1, player)[1]
-        values.append(value)
-        moves.append(move)
-
-    if player == '+':
-        bestVal = max(values)
-    else: #player is -
-        bestVal = min(values)
-    bestMove = moves[values.index(bestVal)]
-    return bestMove, bestVal
-
-def alphaBetaMinimax(game, board, depth, player, alpha, beta):
-#is the game over, return draw or winner
-    terminate = isGameOver(board)
-    if terminate == 0:
-        #player + wins
-        return None, 1
-    if terminate == 1:
-        #player - wins
-        return None, -1
-    if terminate == -1:
-        #draw
-        return None, 0
-
-    if depth == 0:
-        #hit depth game over
-        return None, 0
-
-    values = []
-    moves = []
-    for move in ["U", "D", "L", "R", "S"]:
-        game.makeMove(move)
-        child = game.getGame()
-        value = alphaBetaMinimax(game, child, depth - 1, player, alpha, beta)[1]
-        values.append(value)
-        moves.append(move)
-        if player == '+':
-            if value > alpha:
-                alpha = value
-        elif player == '-':
-            if value < beta:
-                beta = value
-
-        if alpha >= beta:
+    while not game.state() != 0 and len(moves) < max_moves:
+        start = time.time()
+        move = None
+        
+        for result in player1(copy.deepcopy(game)):
+            end = time.time()
+            if end-start > timeout:
+                break
+            else:
+                move = result
+        if not move:
+            print ('player + forfeits the game')
             break
+        else:
+            print ("%s played: %s" % (game.getCurrentPlayer(), move))
 
-    if player == '+':
-        bestVal = max(values)
-    else: #player is -
-        bestVal = min(values)
-    bestMove = moves[values.index(bestVal)]
-    return bestMove, bestVal
+            moves.append((game.getCurrentPlayer(), move))
+            game = game.makeMove(move)
+            
+            game.printGame()
 
+        start = time.time()
+        move = None
+        for result in player2(copy.deepcopy(game)):
+            end = time.time()
+            if end-start > timeout:
+                break
+            else:
+                move = result
+        if not move:
+            print ('player - forfeits the game')
+            break
+        else:
+            print ("%s played: %s" % (game.getCurrentPlayer(), move))
+            
+            moves.append((game.getCurrentPlayer(), move))
+            game = game.makeMove(move)
+            
+            game.printGame()
 
-def next_move(game: Game_IJK) -> None:
+    print('Winner was: ', game.state())
+    return moves
 
-    """board: list of list of strings -> current state of the game
-       current_player: int -> player who will make the next move either ('+') or -'-')
-       deterministic: bool -> either True or False, indicating whether the game is deterministic or not
-    """
+if __name__ == "__main__":
+    if(len(sys.argv) != 4):
+        raise Exception('Please provide three commandline arguments.')
+    
+    (p1, p2, mode) = sys.argv[1:]
+    
+    logics = { "human" : human_IJK.next_move, "ai" : ai_IJK.next_move }
+    deterministic = { "det" : True, "nondet" : False }
 
-    board = game.getGame()
-    player = game.getCurrentPlayer()
-    deterministic = game.getDeterministic()
-
-    move, score = alphaBetaMinimax(game, board, 1000, player, -math.inf, math.inf)
-    yield move
-
-    #yield random.choice(["U", "D", "L", "R", "S"])
+    IJK(logics[p1], logics[p2], deterministic[mode])
