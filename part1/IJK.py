84d0529ccd0b5e6dcc76ea7c6716bdb643bfdd1e
Supe, Neha <nehasupe@iu.edu>
2019-11-08 10:44:23 -0500

    Updated code from Instructors

--- part1/IJK.py
+++ part1/IJK.py
@@ -3,6 +3,8 @@
 """
 @author: Abhilash Kuhikar, October 2019
 
+THIS IS VERSION v2.0, updated 11/1/2019.
+
 ./ijk.py : Interface to an IJK game.
 
 Usage:
@@ -25,9 +27,8 @@ import human_IJK
 
 from logic_IJK import Game_IJK, initialGame
 
-
-def IJK(player1, player2, deterministic, timeout=120000, max_moves=1000):
-    game = initialGame(4, "+", deterministic)
+def IJK(player1, player2, deterministic, timeout=60, max_moves=30000):
+    game = initialGame(6, '+', deterministic)
 
     game.printGame()
 
@@ -35,52 +36,53 @@ def IJK(player1, player2, deterministic, timeout=120000, max_moves=1000):
     while not game.state() != 0 and len(moves) < max_moves:
         start = time.time()
         move = None
-
+        
         for result in player1(copy.deepcopy(game)):
             end = time.time()
-            if end - start > timeout:
+            if end-start > timeout:
                 break
             else:
                 move = result
         if not move:
-            print("player + forfeits the game")
+            print ('player + forfeits the game')
             break
         else:
-            print("%s played: %s" % (game.getCurrentPlayer(), move))
+            print ("%s played: %s" % (game.getCurrentPlayer(), move))
 
             moves.append((game.getCurrentPlayer(), move))
             game = game.makeMove(move)
-
+            
             game.printGame()
 
         start = time.time()
         move = None
         for result in player2(copy.deepcopy(game)):
             end = time.time()
-            if end - start > timeout:
+            if end-start > timeout:
                 break
             else:
                 move = result
         if not move:
-            print("player - forfeits the game")
+            print ('player - forfeits the game')
             break
         else:
-            print("%s played: %s" % (game.getCurrentPlayer(), move))
-
+            print ("%s played: %s" % (game.getCurrentPlayer(), move))
+            
             moves.append((game.getCurrentPlayer(), move))
             game = game.makeMove(move)
-
+            
             game.printGame()
+
+    print('Winner was: ', game.state())
     return moves
 
-
 if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        raise Exception("Please provide three commandline arguments.")
-
+    if(len(sys.argv) != 4):
+        raise Exception('Please provide three commandline arguments.')
+    
     (p1, p2, mode) = sys.argv[1:]
-
-    logics = {"human": human_IJK.next_move, "ai": ai_IJK.next_move}
-    deterministic = {"det": True, "nondet": False}
+    
+    logics = { "human" : human_IJK.next_move, "ai" : ai_IJK.next_move }
+    deterministic = { "det" : True, "nondet" : False }
 
     IJK(logics[p1], logics[p2], deterministic[mode])
